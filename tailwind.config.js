2ba264dacc4559801fa5eaa62166f54e896f9efc
Adrian Diaz Espinoza <adrdiazes@gmail.com>
2025-03-16 20:17:39 -0500

    Fix

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: ["./src/**/*.{astro,js,ts,jsx,tsx}"],
+    theme: {
+      extend: {
+        fontFamily: {
+          inter: ["Inter", "sans-serif"]
+        }
+      }
+    }
+  }
+  
