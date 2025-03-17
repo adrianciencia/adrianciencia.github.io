102bb1a92739c5206c737272e0541ea488aad2c5
Adrian Diaz Espinoza <adrdiazes@gmail.com>
2025-03-17 17:40:16 -0500

    Build

---
+++ src/components/ScrollFade.svelte
@@ -0,0 +1,19 @@
+<script>
+    import { onMount } from 'svelte';
+  
+    let fadeClass = '';
+  
+    onMount(() => {
+      const handleScroll = () => {
+        const scrollPos = window.scrollY;
+        fadeClass = scrollPos > 50 ? 'opacity-0' : 'opacity-100';
+      };
+  
+      window.addEventListener('scroll', handleScroll);
+      return () => window.removeEventListener('scroll', handleScroll);
+    });
+  </script>
+  
+  <div class="transition-opacity duration-500 {fadeClass}">
+    <slot />
+  </div>
---
+++ src/components/ThemeToggle.svelte
@@ -0,0 +1,29 @@
+<script>
+    import { onMount } from 'svelte';
+  
+    let isDark = false;
+  
+    onMount(() => {
+      isDark = document.documentElement.classList.contains('dark');
+    });
+  
+    function toggleTheme() {
+      isDark = !isDark;
+      if (isDark) {
+        document.documentElement.classList.add('dark');
+      } else {
+        document.documentElement.classList.remove('dark');
+      }
+    }
+  </script>
+  
+  <button 
+    on:click={toggleTheme} 
+    class="p-2 rounded border border-gray-300 dark:border-gray-700"
+  >
+    {#if isDark}
+      Light Mode
+    {:else}
+      Dark Mode
+    {/if}
+  </button>
