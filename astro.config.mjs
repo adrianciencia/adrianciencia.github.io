039a8530bf3f4bcfe55b2a59c893ecb02fbcd2d0
Adrian Diaz Espinoza <adrdiazes@gmail.com>
2025-03-17 23:13:03 -0500

    Finally working

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,9 @@ import svelte from '@astrojs/svelte';
 
 export default defineConfig({
   site: 'https://adrianciencia.github.io',
+  base: '/', // Explicitly set the base path
   integrations: [tailwind(), svelte()],
+  output: 'static', // Ensure static output for GitHub Pages
 });
 
 
