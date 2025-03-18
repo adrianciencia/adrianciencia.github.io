a85cca08a5ce0ac1a16f5e2b3825f56c567c3b41
Adrian Diaz Espinoza <adrdiazes@gmail.com>
2025-03-17 19:07:18 -0500

    Local edits: updated styles, pages, and content

---
+++ src/content/blog/myfirstblog.md
@@ -0,0 +1,7 @@
+---
+title: "My First Blog Post"
+date: "2025-03-16"
+description: "Introduction to my blog."
+---
+
+This is my first post. 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class', // Use the 'class' strategy
   content: [
     './src/**/*.{astro,html,js,jsx,ts,tsx,svelte,vue}',
     './public/**/*.html',
